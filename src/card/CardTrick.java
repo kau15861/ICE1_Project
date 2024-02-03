45876faee2aa5d1bdbdd875adb401b8838484b55
kau15861 <kau15861@sheridancollege.ca>
2024-02-03 14:02:17 -0500

    version 2  Harmanpreet Kaur ICE1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -5,8 +5,6 @@
  
 package card;
 
-import java.util.Scanner;
-
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -19,51 +17,40 @@ public class CardTrick {
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
 
-       
-        for (int i = 0; i < magicHand.length; i++) {
-           Card c = new Card();
-           c.setValue(generateRandomNumber(1, 13));
-           c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
-           magicHand[i] = c;
-           System.out.println(c.getSuit() + " " + c.getValue());
+        int i; // Declare i outside of the loop
+
+        for (i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(generateRandomNumber(1, 13));
+            c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
+            magicHand[i] = c;
+            System.out.println(c.getSuit() + " " + c.getValue());
         }
 
         Card luckyCard = new Card();
         luckyCard.setValue(7); // Example value
         luckyCard.setSuit("Hearts"); // Example suit
-        magicHand[i] = luckyCard;
+
+        // Adjust the index to i - 1 since i is incremented after the loop
+        magicHand[i - 1] = luckyCard;
+
         System.out.println(luckyCard.getSuit() + " " + luckyCard.getValue() + " (Lucky Card)");
 
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the value of card  in the range (1-13): ");
-        int user_Card_Value = scanner.nextInt();
-        System.out.print("Enter a suit index (0-Hearts, 1-Diamonds, 2-Clubs, 3-Spades): ");
-        int user_Suit_Index = scanner.nextInt();
-
-       
-        Card user_Card = new Card();
-        user_Card.setValue(user_Card_Value);
-        user_Card.setSuit(Card.SUITS[user_Suit_Index]);
-
-       
-        boolean isUser_CardInMagicHand = false;
+        boolean isLuckyCardInMagicHand = false;
         for (Card c : magicHand) {
-            if (c.getValue() == user_Card.getValue() && c.getSuit().equalsIgnoreCase(user_Card.getSuit())) {
-                isUser_CardInMagicHand = true;
+            if (c.getValue() == luckyCard.getValue() && c.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                isLuckyCardInMagicHand = true;
                 break;
             }
         }
 
-        
-        if (isUser_CardInMagicHand) {
+        if (isLuckyCardInMagicHand) {
             System.out.println("Well done! Your chosen card is part of the magic hand!");
         } else {
             System.out.println("Unfortunately, your selected card is not among the magic hand's cards.");
         }
     }
 
-   
     private static int generateRandomNumber(int min, int max) {
         return min + (int) (Math.random() * ((max - min) + 1));
     }
